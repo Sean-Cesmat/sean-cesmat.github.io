cf7213c5684e149017f559fd43ead984e2ee20e5
Sean Cesmat <sean.cesmat@gmail.com>
2018-04-06 00:23:52 -0700

    Pushing Live - still missing project 4

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # sean-cesmat.github.io
 Root of GitPages
+
+#### Credits
+Light Gallery - http://sachinchoolur.github.io/lightGallery/docs/api.html
+Skills Bar - https://codepen.io/tamak/pen/hzEer
--- js/site.js
+++ js/site.js
@@ -1,8 +1,43 @@
 $(document).ready(function() {
   $("#jf-lightgallery").lightGallery();
+  $("#whitecap-lightgallery").lightGallery();
 
-  $('.button-collapse').sideNav({
+  $('.sidenav').sidenav({
        edge: 'right', // Choose the horizontal origin
      }
    );
+
+  jQuery('.skillbar').each(function(){
+ 		jQuery(this).find('.skillbar-bar').animate({
+ 			width:jQuery(this).attr('data-percent')
+ 		},6000);
+ 	});
+
+  $('.scrollspy').scrollSpy();
+
+
+  if (jQuery(window).width() < 992) {
+    jQuery('.swap2').each(function () {
+      if (!jQuery(this).text().match(/^\s*jQuery/)) {
+        jQuery(this).insertBefore(jQuery(this).prev('.swap1'));
+      }
+    });
+  }
+  jQuery(window).resize(function() {
+    if (jQuery(window).width() < 992) {
+      jQuery('.swap2').each(function () {
+          if (!jQuery(this).text().match(/^\s*jQuery/)) {
+              jQuery(this).insertBefore(jQuery(this).prev('.swap1'));
+          }
+      });
+    }
+    else {
+      jQuery('.swap1').each(function () {
+          if (!jQuery(this).text().match(/^\s*jQuery/)) {
+              jQuery(this).insertBefore(jQuery(this).prev('.swap2'));
+          }
+      });
+    }
+  });
+
 });
